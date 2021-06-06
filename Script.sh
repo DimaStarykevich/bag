6601ae58beda9fee46883d5bcb0921b22f372b5d
DimaStarykevich <83612586+DimaStarykevich@users.noreply.github.com>
2021-06-06 13:36:50 +0300

    Add files via upload

---
+++ Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls Name
+mkdir {1,2,3}
+ls
+touch document.txt document1.txt document2.txt document.json document1.json
+mkdir {1,2,3}
+ls-| Name
+mv document1.txt document2.txt /Name1
+
+
